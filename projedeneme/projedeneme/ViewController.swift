976cdfa8b055b0d819dde4c920290ab34a5e49a4
Gizemnurozden <gizemnurozden1@gmail.com>
2023-12-12 20:17:13 +0300

    Projedenemesi
    Projedenemesi

---
+++ projedeneme/projedeneme/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  projedeneme
+//
+//  Created by Gizemnur Özden on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func buttontikla(_ sender: Any) {
+    }
+}
+
